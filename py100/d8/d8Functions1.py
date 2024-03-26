01c21f07ebe82e39e095111fb6cb802dcaa01c58
MahmoudGharib <mahmoud.ia.gharib@gmail.com>
2024-03-26 11:42:31 +0100

    day 8 examples of 100 days python

---
+++ py100/d8/d8Functions1.py
@@ -0,0 +1,21 @@
+# this module will focus on creating functions 
+
+#create a function 
+def d8_greetFunction ():
+    print ("Hello")
+    Name1 = input ("please enter your name ")
+    print ("Hello " + Name1 )
+
+#greeting with name input 
+def d8_greetWithName (inputName):
+    print (f'Hello {inputName}')
+    print (f'How are you {inputName} ?')    
+
+#welcome the customer in name in Cityx
+    # user Name (string)
+    # City name (String)
+def d8_WelcomeTo (Name, City):
+    print (f'welcome to {City}') 
+    print (f'we wish a pleasent stay {Name} ')
+    print (" remeber the principle of positional Arguments for Muliple inputs")    
+    print (" we can also use the Keyword argument (Name = 'Dodo', City = 'Giza')\n")   
---
+++ py100/d8/d8Main.py
@@ -0,0 +1,6 @@
+import d8Functions1
+
+#d8Functions1.d8_greetFunction()
+#d8Functions1.d8_greetWithName("Dodo")
+d8Functions1.d8_WelcomeTo("Moudi", "Cairo")
+d8Functions1.d8_WelcomeTo(City="Giza", Name="Dodo")
